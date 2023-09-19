ed3cb3ffe6017e9c0dbc740de7f540f445415512
itsalaassar <itsalaassar@gmail.com>
2023-09-19 17:29:04 +0300

    Category Graphql

--- src/Modules/Category/GraphQl/GraphQlCategoryResolvers.js
+++ src/Modules/Category/GraphQl/GraphQlCategoryResolvers.js
@@ -1,9 +1,37 @@
 
-import{GraphQLString}from'graphql'
+import{GraphQLList, GraphQLObjectType, GraphQLString}from'graphql'
+import { CategoryModel } from '../../../../DB/Models/Category.model.js'
+
+const ImageType= new GraphQLObjectType({
+
+                name:'imageType',
+                fields:{
+                    secure_url:{type:GraphQLString},
+                    public_id:{type:GraphQLString}
+                },
+            })
+
+
+const CategoryType = new GraphQLObjectType({
+    name:'CategoryType',
+        fields:{
+            name:{type:GraphQLString},
+            slug:{type:GraphQLString},
+            Image:{type:ImageType},
+            createdBy:{type:GraphQLString},
+            updatedBy:{type:GraphQLString},
+            CustomId:{type:GraphQLString}
+
+        }
+})
+
+
+
 export const GetAllCategoryResolvers={
-    type:GraphQLString,
-    resolve:()=>{
-        return "helloCategory"
+    type:new GraphQLList(CategoryType),
+    resolve: async()=>{
+        const Categories =await CategoryModel.find({})
+        return Categories
 
     }
 }
